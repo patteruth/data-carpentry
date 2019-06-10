cb60632f5cf354e1b6b69aae3d73f348cbf71690
Ruth Patterson <patterson.ruth@gmail.com>
2019-06-10 12:09:59 +0930

    ordered imports according to PEP-8

--- plot_precipitation_climatology.py
+++ plot_precipitation_climatology.py
@@ -1,9 +1,11 @@
 import argparse
-import xarray as xr
-import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
+
 import numpy as np
+import matplotlib.pyplot as plt
+import xarray as xr
+import cartopy.crs as ccrs
 import cmocean
+
 import unit_conversion
 
 def create_plot(clim, model_name, season, gridlines=False):
